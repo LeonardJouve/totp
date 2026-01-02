8646b3e5e6641826ef52b0e414a7083fb6578daf
Léonard Jouve <leonard.jouve@gmail.com>
2026-01-02 16:39:01 +0100

    release executable

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import {base32} from "@scure/base";
-import {print} from "./hotp";
+import {spawn} from "node:child_process";
+import {totp} from "./hotp";
+
+if (process.argv.length < 3) {
+    throw new Error(`usage: "${process.argv[1]} <secret>"`);
+}
 
 // Authenticator app take a base32 encoded secret
-const secret = base32.decode("I65VU7K5ZQL7WB4E");
+const base32Secret = process.argv[2];
+const secret = base32.decode(base32Secret);
 const digits = 6;
 
-print(secret, digits);
+const result = totp(secret, digits);
+spawn("clip").stdin.end(result);
+console.log("Done");
